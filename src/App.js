d10cb04d5f8a95769eefc37ef3d58805cfb303fe
stanflows <atakistanley@gmail.com>
2022-01-18 11:22:01 +0100

    Added new features

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Header"
 import Landing from "./components/Landing";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 import "./generic.css";
 
 import data from "./data";
@@ -11,14 +11,6 @@ function App() {
     <div className="card">
       <Header />
       <Landing />
-
-      <Footer 
-            twitter={data.social.twitter}
-            facebook={data.social.facebook}
-            instagram={data.social.instagram}
-            linkedIn={data.social.linkedIn}
-            github={data.social.github} 
-      />
     </div>
   );
 }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,6 @@
 import About from "./About";
 import Interests from "./Interests";
+import Footer from "./Footer";
 
 import data from "../data";
 
@@ -8,6 +9,14 @@ const Landing = () => {
         <div className="landing_page">
             <About aboutText={data.about} />
             <Interests interestsText={data.interests} />
+            
+            <Footer 
+                twitter={data.social.twitter}
+                facebook={data.social.facebook}
+                instagram={data.social.instagram}
+                linkedIn={data.social.linkedIn}
+                github={data.social.github} 
+            />
         </div>
     )
 }
