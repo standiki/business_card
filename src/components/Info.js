6e3f3012dfa3ed2d31c16e96cf20ecf8b38282a4
stanflows <atakistanley@gmail.com>
2022-02-03 10:27:04 +0100

    refactored

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,18 +7,18 @@ import data from "../data";
 const Info = () => {
     return (
         <div className="info_container">
-            <img src={process.env.PUBLIC_URL + data.profileImage} alt={data.title} className="info_img" />
+            <img src={process.env.PUBLIC_URL + data.profileImage} alt={data.name} className="info_img" />
             <div className="info">
                 <h3 className="info_name">{data.name}</h3>
                 <p className="info_title">{data.title}</p>
                 <a href={data.portfolio} target="_blank" rel="noreferrer" className="info_link" >Take a tour</a>
             </div>
-            <div className="info_social">
-                <button className="email">
+            <div className="info_btn">
+                <button className="email_btn">
                     <FontAwesomeIcon icon={faEnvelope} className="fa_icon"></FontAwesomeIcon>
                     email
                 </button>
-                <button className="linkInd">
+                <button className="linkInd_btn">
                     <FontAwesomeIcon icon={faLinkedin} className="fa_icon"></FontAwesomeIcon>
                     linkedIn
                 </button>
